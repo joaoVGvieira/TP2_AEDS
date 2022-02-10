bf55d77d7f62efcdbbf2a13ebb51972f09749033
Iury Martins Pereira <iurymartins46@gmail.com>
2022-02-09 22:22:26 -0300

    melhor solucao

--- Sources/solucao.c
+++ Sources/solucao.c
@@ -10,124 +10,6 @@ int get_menor_distancia(Solucao *solucao){
     return solucao->menor_distancia;
 }
 
-void melhor_solucao(Dados_Cidades *dados_cidades, Arranjos *arranjos, Solucao *solucao){
-    int arranjos_uteis = get_arranjos_uteis(arranjos);
-    int N = get_N(dados_cidades);
-    int *vetor_solucao;
-    vetor_solucao = (int*)malloc((3*N) * sizeof(int));
-    int *vetor_aux;
-    vetor_aux = (int*)calloc((N+2),sizeof(int));
-    int l,c,i,j, distancia, aux1, aux2, posi;
-    for(i = 0; i < arranjos_uteis; i++){
-        for(j = 0; j < N; j++){
-            set_vetor_Q_marcador(dados_cidades, j, 0);
-        }
-        posi = 0;
-        distancia = 0;
-        for(j = 0; j <= (N + 2); j++){
-            aux1 = get_arranjos(arranjos, i, j);
-            aux2 = get_arranjos(arranjos, i, j+1);
-            vetor_solucao[posi] = aux1;
-            posi++;
-            if(aux2 == -1){
-                break;
-            }
-            distancia += get_vetor_M(dados_cidades, aux1, aux2);
-            set_vetor_Q_marcador(dados_cidades, aux1, 1);
-        }
-        vetor_solucao[posi] = -1;
-        for(c = 0; c < arranjos_uteis; c++){
-            int posi1 = posi;
-            int dist1 = distancia;
-            int qual = 0;
-            int recomeca;
-            int sair = 0;
-            l = 0;
-            while(1){
-                if(l >= arranjos_uteis){
-                    break;
-                }
-                recomeca = 0;
-                if(qual == 0){
-                    qual = 1;
-                    for(j = 0; j <= (N + 2); j++){
-                        aux1 = get_arranjos(arranjos, c, j);
-                        vetor_aux[j] = aux1;
-                        if(aux1 == -1){
-                            break;
-                        }
-                    }
-                }
-                else{
-                    for(j = 0; j <= (N + 2); j++){
-                        aux1 = get_arranjos(arranjos, l, j);
-                        vetor_aux[j] = aux1;
-                        if(aux1 == -1){
-                            break;
-                        }
-                    }
-                    l++;
-                }
-                int a;
-                for(j = 0; j <= (N + 2); j++){
-                    aux1 = vetor_aux[j];
-                    if(aux1 == -1){
-                        break;
-                    }
-                    a = get_vetor_Q_marcador(dados_cidades, aux1);
-                    if(aux1 != 0 && a == 1){
-                        recomeca = 1;
-                        break;
-                    }
-                }    
-                if(recomeca == 0){
-                    l = 0;
-                    for(j = 0; j <= (N + 2); j++){
-                        aux1 = vetor_aux[j];
-                        aux2 = vetor_aux[j+1];
-                        vetor_solucao[posi1] = aux1;
-                        posi1++;
-                        if(aux2 == -1){
-                            break;
-                        }
-                        dist1 += get_vetor_M(dados_cidades, aux1, aux2);
-                        set_vetor_Q_marcador(dados_cidades, aux1, 1);
-                    }
-                    vetor_solucao[posi1] = -1;
-                    if(verifica_marcadores(dados_cidades) == 1){
-                        sair = 1;
-                        if(get_menor_distancia(solucao) == 0){
-                            set_menor_distancia(solucao, dist1);
-                            for(j = 0; j < (3*N); j++){
-                                aux1 = vetor_solucao[j];
-                                solucao->melhor_solucao[j] = aux1;
-                                if(aux1 == -1){
-                                    break;
-                                }
-                            }
-                        }
-                        else if(dist1 < (get_menor_distancia(solucao))){
-                            set_menor_distancia(solucao, 0);
-                            set_menor_distancia(solucao, dist1);
-                            for(j = 0; j < (3*N); j++){
-                                aux1 = vetor_solucao[j];
-                                solucao->melhor_solucao[j] = aux1;
-                                if(aux1 == -1){
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                if(sair == 1){
-                    break;
-                }
-            }
-        }
-    }
-    free(vetor_solucao);
-    free(vetor_aux);
-}
 int verifica_marcadores(Dados_Cidades *dados_cidades){
     int N = get_N(dados_cidades);
     for(int i = 1; i < N; i++){
@@ -138,28 +20,6 @@ int verifica_marcadores(Dados_Cidades *dados_cidades){
     return 1;
 }
 
-void imprime_melhor_solucao(Dados_Cidades *dados_cidades, Solucao *solucao){
-    printf("\nS=[ ");
-    int N = get_N(dados_cidades);
-    int aux1, aux2, i;
-    aux1 = solucao->melhor_solucao[i];
-    printf("\nS=[ ");
-    for(i = 0; i < (3*N); i++){
-        aux1 = solucao->melhor_solucao[i];
-        if(aux1 == -1){
-            break;
-        }
-        aux2 = solucao->melhor_solucao[i+1];
-        if(aux1 == 0 && aux2 == 0){
-            printf("");
-        }
-        else{
-            printf("%d ", aux1);
-        }
-    }
-    printf("]\n");
-}
-/*
 void melhor_solucao(Dados_Cidades *dados_cidades, Arranjos *arranjos, Solucao *solucao){
     int N = get_N(dados_cidades);
     int arranjos_uteis = get_arranjos_uteis(arranjos);
@@ -255,7 +115,7 @@ void melhor_solucao(Dados_Cidades *dados_cidades, Arranjos *arranjos, Solucao *s
     free(num);
 }
 void imprime_melhor_solucao(Dados_Cidades *dados_cidades, Solucao *solucao){
-    printf("S=[ ");
+    printf("\nS=[ ");
     int i = 0;
     int aux;
     while (1){
@@ -269,4 +129,3 @@ void imprime_melhor_solucao(Dados_Cidades *dados_cidades, Solucao *solucao){
     
     printf("]\n");
 }
-*/
